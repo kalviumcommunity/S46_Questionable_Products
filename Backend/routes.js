206bb36c421abaab05509e37809017d10ca9242c
Melvin0070 <melvin.aspirant@gmail.com>
2024-02-19 11:50:08 +0530

    Rendered components using backend entities

--- Backend/routes.js
+++ Backend/routes.js
@@ -8,8 +8,6 @@ const { User, Product } = require('./schema');
 
 router.post('/users', async (req, res) => {
     const user = new User({
-        name: req.body.name,
-        userID: req.body.userID,
         email: req.body.email,
         password: req.body.password
     });
@@ -24,10 +22,11 @@ router.post('/users', async (req, res) => {
 
 router.post('/products', async (req, res) => {
     const product = new Product({
-        name: req.body.name,
-        productID: req.body.productID,
+        title: req.body.title,
+        image: req.body.image,
         description: req.body.description,
-        category: req.body.category
+        category: req.body.category,
+        votes: req.body.votes,
     });
 
     try {
@@ -44,7 +43,7 @@ router.post('/products', async (req, res) => {
 
 // getting a user and product (R)
 
-router.get('/getuser', async (req, res) => {
+router.get('/user', async (req, res) => {
     try {
         const users = await User.find();
         res.json(users);
@@ -73,9 +72,6 @@ router.patch('/users/:id', async (req, res) => {
         if (user == null) {
             return res.status(404).json({ message: 'User not found' });
         }
-        if (req.body.name != null) {
-            user.name = req.body.name;
-        }
         if (req.body.email != null) {
             user.email = req.body.email;
         }
@@ -98,14 +94,20 @@ router.patch('/products/:id', async (req, res) => {
         if (product == null) {
             return res.status(404).json({ message: 'Product not found' });
         }
-        if (req.body.name != null) {
-            product.name = req.body.name;
+        if (req.body.image != null) {
+            product.image = req.body.image;
+        }
+        if (req.body.title != null) {
+            product.title = req.body.title;
         }
         if (req.body.description != null) {
             product.description = req.body.description;
         }
         if (req.body.category != null) {
             product.category = req.body.category;
+        }
+        if (req.body.votes != null) {
+            product.votes = req.body.votes;
         }
 
         const updatedProduct = await product.save();
--- client/questionable-products/src/components/Home.jsx
+++ client/questionable-products/src/components/Home.jsx
@@ -1,17 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Home.css'
 import Navbar from './Navbar'
 import Post from './Post'
+import axios from 'axios'
+
 
 function Home() {
+  const [data, setData] = useState()
+  const [isLoading, setLoading] = useState(true)
+
+  useEffect(()=>{
+    axios.get("http://localhost:3000/products")
+    .then(data=>{
+      setData(data.data)
+      console.log(data.data)
+      setTimeout(() => {
+        setLoading(false)
+      }, 1000);
+    })
+
+  },[])
+
+
+  if (isLoading) {
+    return(
+      <div className='flexx'>
+      <svg viewBox="25 25 50 50">
+        <circle r="20" cy="50" cx="50"></circle>
+      </svg>
+      </div>)
+}
 
   return (
     <div>
         <Navbar />
         <button className='posts'>Create Post</button>
         <div className='posts-container'>
-        <Post />
-        <Post />
+        {
+          data && data.map(post=>{
+            return <Post {...post} key={post.id}/>
+          } 
+          )}
         </div>
     </div>
   )
