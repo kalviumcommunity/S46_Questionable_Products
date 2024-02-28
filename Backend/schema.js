f3deb69e318a8dae19e816c9ad7b727aa06d401b
Melvin0070 <melvin.aspirant@gmail.com>
2024-02-28 16:18:23 +0530

    Request validation using joi

--- Backend/schema.js
+++ Backend/schema.js
@@ -1,36 +1,39 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    image: {
-        type: String,
-    },
-    description: {
-        type: String,
-    },
-    category: {
-        type: String,
-    },
-    votes:{
-        type: Number,
-    }
-})
+  title: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  category: {
+    type: String,
+  },
+  votes: {
+    type: Number,
+  },
+});
 
 const userSchema = new mongoose.Schema({
-    email: {
-        type: String,        
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    }
-})
-const User = mongoose.model('users', userSchema)
-const Product = mongoose.model('products', productSchema) 
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+const User = mongoose.model("users", userSchema);
+const Product = mongoose.model("products", productSchema);
 
-
-module.exports = { User, Product }
+module.exports = { User, Product };
--- client/questionable-products/src/components/Signup.jsx
+++ client/questionable-products/src/components/Signup.jsx
@@ -1,11 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Signup.css";
 import imgg from "../assets/24237566_6922095.jpg";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useForm } from "react-hook-form";
+import axios from "axios";
 
 function Signup() {
+  const navigate = useNavigate();
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const handleSignup = async (data) => {
+    if (data.password !== data.confirm) {
+      toast.error("Passwords do not match");
+    } else {
+      try {
+        const response = await axios.post("https://questionable-products.onrender.com/users", {
+          username: data.Username,
+          email: data.email,
+          password: data.password,
+          confirmPassword: data.confirm,
+        });
+        toast.success("Signup Successful");
+        setTimeout(() => {
+          navigate("/");
+        }, 3000);
+      } catch (err) {
+        console.log(err);
+        toast.error("Signup Failed");
+      }
+    }
+  };
+
   return (
     <div className="wrapper">
+      <ToastContainer />
       <div className="left">
         <img src={imgg} alt="" />
       </div>
@@ -13,17 +48,83 @@ function Signup() {
         <h1 className="top">
           questionable <br></br>products
         </h1>
-        <div className="form">
-          <h2>Explore mind boggling products</h2>
-          <input
-            className="username"
-            type="text"
-            placeholder="username/email"
-          />
-          <input className="password" type="password" placeholder="password" />
-          <Link to="/">
-            <button className="signup">Sign Up</button>
-          </Link>
+        <div>
+          <h2>Explore mind-boggling products</h2>
+          <form onSubmit={handleSubmit(handleSignup)}>
+            <input
+              className="username"
+              placeholder="Username"
+              type="text"
+              {...register("Username", {
+                required: "Username is required!",
+                minLength: {
+                  value: 3,
+                  message: "Username should be more than 3 characters",
+                },
+                maxLength: {
+                  value: 30,
+                  message: "Username should be less than 30 characters",
+                },
+              })}
+            />
+            {errors.Username && <p>{errors.Username.message}</p>}
+
+            <input
+              className="username"
+              placeholder="Email"
+              type="text"
+              {...register("email", {
+                required: "Email is required!",
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                  message: "Invalid email",
+                },
+              })}
+            />
+            {errors.email && <p>{errors.email.message}</p>}
+
+            <input
+              className="password"
+              type="password"
+              placeholder="Password"
+              {...register("password", {
+                required: "Password is required!",
+                pattern: {
+                  value: /.*[\W]+.*/i,
+                  message:
+                    "Password must contain at least one special character",
+                },
+                minLength: {
+                  value: 5,
+                  message: "Password must have at least 5 characters",
+                },
+                maxLength: {
+                  value: 20,
+                  message: "Password must have at most 20 characters",
+                },
+              })}
+            />
+            {errors.password && <p>{errors.password.message}</p>}
+
+            <input
+              className="password"
+              type="password"
+              placeholder="Confirm Password"
+              {...register("confirm", {
+                required: "Confirm your password",
+                validate: (value) =>
+                  value === watch("password") || "Passwords don't match",
+              })}
+            />
+            {errors.confirm && <p>{errors.confirm.message}</p>}
+
+            <button className="signup" type="submit">
+              Sign Up
+            </button>
+          </form>
+          <p>
+            Already have an account? <Link to="/login">Login</Link>
+          </p>
         </div>
       </div>
     </div>
